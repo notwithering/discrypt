b3b4309b91cf8014d67d213f307f3e26bbe654a7
Core <124115470+NotWithering@users.noreply.github.com>
2023-12-08 18:32:37 -0500

    change token

--- config.go
+++ config.go
@@ -5,7 +5,7 @@ var Config = struct {
 	Channels channels
 	Client   client
 }{
-	Token: "MTEyNjc0NDg5OTk5NjM1NjYzOA.GRE7AZ.J-oHnoeINmGrGUYwW-GOXLbNGSdE2TLPhBWl3k",
+	Token: "YOUR_TOKEN_HERE",
 	Channels: channels{
 		Messaging: "1127831380567523408",
 	},
